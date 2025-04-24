d38d3e7dd12afd83b0310be0d20bc85ea64641be
fahmiitmamul <itmamul2004@gmail.com>
2025-04-24 23:20:13 +0700

    update frontend

--- app/page.js
+++ app/page.js
@@ -1,10 +1,159 @@
 import Sidebar from "@/components/sidebar-wrapper";
 import AppHeader from "@/components/app-header";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { Info } from "lucide-react";
+import { Speaker } from "lucide-react";
+import { SpeakerIcon } from "lucide-react";
+import { Volume2 } from "lucide-react";
+import { LayoutTemplate } from "lucide-react";
+import { UsersRound } from "lucide-react";
 
 export default function Page() {
   return (
     <Sidebar>
-      <AppHeader />
+      <div className="flex flex-col w-full">
+        {/* Header */}
+        <div className="w-full">
+          <AppHeader />
+        </div>
+
+        {/* Content */}
+        <div className="p-5">
+          <div>
+            <Card className="w-full border-l-8">
+              <CardHeader>
+                <CardTitle>
+                  <h1 className="uppercase font-bold tracking-light">
+                    Selamat Datang,
+                  </h1>
+                </CardTitle>
+                <CardDescription>
+                  <h1 className="uppercase font-bold text-xl tracking-light">
+                    Itmamul Fahmi
+                  </h1>
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </div>
+
+          <div className="grid grid-cols-1 xl:grid-cols-2 pt-10 gap-5">
+            <div className="w-full">
+              <Card className="w-full border-l-8">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex gap-2 items-center">
+                      <div>
+                        <Info />
+                      </div>
+                      <h1 className="uppercase font-bold tracking-light text-xl">
+                        Info
+                      </h1>
+                    </div>
+                  </CardTitle>
+                  <CardDescription>
+                    <h1 className="text-sm tracking-light">
+                      Saat ini masa pendataan sedang dibuka.{" "}
+                      <Link href="/" className="text-green-500 font-medium">
+                        Baca Selengkapnya
+                      </Link>
+                    </h1>
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+            <div className="w-full">
+              <Card className="w-full border-l-8">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex gap-2 items-center">
+                      <div>
+                        <Volume2 />
+                      </div>
+                      <h1 className="uppercase font-bold tracking-light text-xl">
+                        Pengumuman (0)
+                      </h1>
+                    </div>
+                  </CardTitle>
+                  <CardDescription>
+                    <h1 className="text-sm tracking-light">
+                      Fitur baru dibuka.{" "}
+                      <Link href="/" className="text-green-500 font-medium">
+                        Baca Selengkapnya
+                      </Link>
+                    </h1>
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pt-10 gap-5">
+            <div className="w-full">
+              <Card className="w-full border-l-8">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex gap-2 items-center">
+                      <div>
+                        <LayoutTemplate />
+                      </div>
+                      <h1 className="uppercase font-bold tracking-light text-xl">
+                        2
+                      </h1>
+                    </div>
+                  </CardTitle>
+                  <CardDescription>
+                    <h1 className="text-sm tracking-light">Ruangan</h1>
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+            <div className="w-full">
+              <Card className="w-full border-l-8">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex gap-2 items-center">
+                      <div>
+                        <UsersRound />
+                      </div>
+                      <h1 className="uppercase font-bold tracking-light text-xl">
+                        10
+                      </h1>
+                    </div>
+                  </CardTitle>
+                  <CardDescription>
+                    <h1 className="text-sm tracking-light">Santri</h1>
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+            <div className="w-full">
+              <Card className="w-full border-l-8">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex gap-2 items-center">
+                      <div>
+                        <UsersRound />
+                      </div>
+                      <h1 className="uppercase font-bold tracking-light text-xl">
+                        5
+                      </h1>
+                    </div>
+                  </CardTitle>
+                  <CardDescription>
+                    <h1 className="text-sm tracking-light">Ustadz</h1>
+                  </CardDescription>
+                </CardHeader>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
     </Sidebar>
   );
 }
--- components/app-header.jsx
+++ components/app-header.jsx
@@ -12,27 +12,12 @@ import { Sun } from "lucide-react";
 import { Button } from "./ui/button";
 import { Moon } from "lucide-react";
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu";
-import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
   DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { User2 } from "lucide-react";
@@ -41,79 +26,77 @@ export default function AppHeader() {
   const { setTheme, theme } = useTheme();
 
   return (
-    <div className="w-full">
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-        <div className="w-full flex justify-between">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+    <header className="sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <div className="w-full flex justify-between">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+        <div className="flex flex-wrap gap-2 mr-5">
+          <div>
+            <Button
+              variant="outline"
+              size="icon"
+              className="cursor-pointer"
+              onClick={() => {
+                if (theme === "light") {
+                  setTheme("dark");
+                } else {
+                  setTheme("light");
+                }
+              }}
+            >
+              {theme === "light" ? (
+                <Sun className="h-5 w-5" />
+              ) : (
+                <Moon className="h-5 w-5" />
+              )}
+            </Button>
           </div>
-          <div className="flex flex-wrap gap-2 mr-5">
-            <div>
-              <Button
-                variant="outline"
-                size="icon"
-                className="cursor-pointer"
-                onClick={() => {
-                  if (theme === "light") {
-                    setTheme("dark");
-                  } else {
-                    setTheme("light");
-                  }
-                }}
-              >
-                {theme === "light" ? (
-                  <Sun className="h-5 w-5" />
-                ) : (
-                  <Moon className="h-5 w-5" />
-                )}
-              </Button>
-            </div>
-            <div>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline" className="cursor-pointer">
-                    Itmamul Fahmi
-                    <User2 className="ml-2 h-4 w-4" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56">
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>
-                      Profile
-                      <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      Daftar Akun
-                      <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      Daftar Akses Lembaga
-                      <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      Ganti Password
-                      <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                  <DropdownMenuSeparator />
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="cursor-pointer">
+                  Itmamul Fahmi
+                  <User2 className="ml-2 h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuGroup>
                   <DropdownMenuItem>
-                    Log out
-                    <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                    Profile
+                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
                   </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
+                  <DropdownMenuItem>
+                    Daftar Akun
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Daftar Akses Lembaga
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Ganti Password
+                    <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  Log out
+                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+    </header>
   );
 }
