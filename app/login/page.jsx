202f39440f21b716d2f89f159008c55c9b605b45
fahmiitmamul <itmamul2004@gmail.com>
2025-04-24 20:13:37 +0700

    update

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,17 +1,18 @@
 import { LoginForm } from "@/components/login-form";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
+          <Link href="/" className="flex items-center gap-2 font-medium">
             <div>
               <Image src="/darul-hikam.png" width={30} height={30} alt="Logo" />
             </div>
             DARUL HIKAM TPQ
-          </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
