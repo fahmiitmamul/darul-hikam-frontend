02dc449ad2fc8542dfced0216b3d926dc7cab7a6
fahmiitmamul <itmamul2004@gmail.com>
2025-06-09 10:01:59 +0700

    fix: update

--- app/(kelembagaan)/mudir-atau-pimpinan/page.jsx
+++ app/(kelembagaan)/mudir-atau-pimpinan/page.jsx
@@ -1,10 +1,11 @@
 "use client";
+
+import React, { useState } from "react";
 import Sidebar from "@/components/sidebar-wrapper";
 import AppHeader from "@/components/app-header";
 import {
   flexRender,
   getCoreRowModel,
-  getSortedRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
   useReactTable,
@@ -18,249 +19,53 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import React from "react";
-import { ChevronsLeft } from "lucide-react";
-import { ChevronLeft } from "lucide-react";
-import { ChevronRight } from "lucide-react";
-import { ChevronsRight } from "lucide-react";
+import {
+  ChevronsLeft,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsRight,
+  MoreHorizontal,
+  Trash,
+  Pencil,
+} from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MoreHorizontal } from "lucide-react";
-import { Trash } from "lucide-react";
-import { Pencil } from "lucide-react";
+
 import ModalTambahMudirAtauPimpinan from "@/components/(kelembagaan)/mudir-atau-pimpinan/modal-tambah-mudir-atau-pimpinan/page";
-import { useState } from "react";
 import { ModalHapusMudirAtauPimpinan } from "@/components/(kelembagaan)/mudir-atau-pimpinan/modal-hapus-mudir-atau-pimpinan/page";
 import http from "@/helpers/http.helper";
 import { useQuery } from "@tanstack/react-query";
 
 export default function MudirAtauPimpinan() {
-  const [openDialogEditMudirAtauPimpinan, setOpenDialogEditMudirAtauPimpinan] =
-    useState(false);
-  const [
-    openDialogHapusMudirAtauPimpinan,
-    setOpenDialogHapusMudirAtauPimpinan,
-  ] = useState(false);
-  const [allContractPage, setAllContractPage] = useState(1);
-  const [allContractLimit, setAllContractLimit] = useState(5);
-  const [allContractSearchData, setAllContractSearchData] = useState("");
+  const [openDialogEdit, setOpenDialogEdit] = useState(false);
+  const [openDialogHapus, setOpenDialogHapus] = useState(false);
 
-  async function fetchMudirAtauPimpinan(pageData, search, limitData) {
+  const [pageIndex, setPageIndex] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const [globalFilter, setGlobalFilter] = useState("");
+
+  const getDataMudirPimpinan = async (page, limit, search) => {
     const { data } = await http().get(
-      "/mudir-atau-pimpinan?page=" +
-        pageData +
-        "&search=" +
-        search +
-        "&limit=" +
-        limitData
+      `/mudir-atau-pimpinan?page=${page}&limit=${limit}&search=${search}`
     );
     return data.results;
-  }
+  };
 
-  const { data: mudirAtauPimpinanData } = useQuery({
-    queryKey: [
-      "all-contract",
-      allContractPage,
-      allContractSearchData,
-      allContractLimit,
-    ],
-    queryFn: () =>
-      fetchMudirAtauPimpinan(
-        allContractPage,
-        allContractSearchData,
-        allContractLimit
-      ),
-    staleTime: 10 * 60 * 1000,
-    cacheTime: 60 * 60 * 1000,
+  const { data } = useQuery({
+    queryKey: ["mudir-pimpinan", pageIndex, pageSize, globalFilter],
+    queryFn: () => getDataMudirPimpinan(pageIndex, pageSize, globalFilter),
+    keepPreviousData: true,
   });
 
-  const tableData = [
-    {
-      no: "1",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "2",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "3",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "4",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "5",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "6",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "7",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "8",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "9",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "10",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "11",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "12",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "13",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "14",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "15",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "16",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "17",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "18",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "19",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "20",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "21",
-      nama_lengkap: "983457345",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-12-2023",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-    {
-      no: "22",
-      nama_lengkap: "987654678",
-      no_handphone: "31-12-2022",
-      tanggal_sk: "31-05-2000",
-      status_keaktifan: "Aktif",
-      aksi: "Aksi",
-    },
-  ];
-
-  const tableColumns = [
+  const columns = [
     {
       accessorKey: "no",
       header: "No",
+      cell: ({ row }) => row.index + 1 + (pageIndex - 1) * pageSize,
     },
     {
       accessorKey: "nama_lengkap",
@@ -273,38 +78,32 @@ export default function MudirAtauPimpinan() {
     {
       accessorKey: "tanggal_sk",
       header: "Tanggal SK",
+      cell: () => "-", // placeholder jika tidak ada field
     },
     {
       accessorKey: "status_keaktifan",
       header: "Status Keaktifan",
     },
     {
-      accessorKey: "Aksi",
+      accessorKey: "aksi",
       header: "Aksi",
       cell: ({ row }) => (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0 cursor-pointer">
+            <Button variant="ghost" className="h-8 w-8 p-0">
               <MoreHorizontal />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuItem
-              className="cursor-pointer"
-              onClick={() => {
-                setOpenDialogEditMudirAtauPimpinan(true);
-              }}
-            >
-              <Pencil />
+            <DropdownMenuItem onClick={() => setOpenDialogEdit(true)}>
+              <Pencil className="mr-2 h-4 w-4" />
               Edit
             </DropdownMenuItem>
             <DropdownMenuItem
-              className="text-red-500 cursor-pointer"
-              onClick={() => {
-                setOpenDialogHapusMudirAtauPimpinan(true);
-              }}
+              onClick={() => setOpenDialogHapus(true)}
+              className="text-red-600"
             >
-              <Trash />
+              <Trash className="mr-2 h-4 w-4" />
               Delete
             </DropdownMenuItem>
           </DropdownMenuContent>
@@ -313,130 +112,108 @@ export default function MudirAtauPimpinan() {
     },
   ];
 
-  const [data] = React.useState(() => [...tableData]);
-  const [columns] = React.useState(() => [...tableColumns]);
-
-  const [pagination, setPagination] = React.useState({
-    pageIndex: 0,
-    pageSize: 5,
-  });
-
   const table = useReactTable({
-    data,
+    data: data?.data ?? [],
     columns,
+    pageCount: data?.totalPages ?? -1,
+    state: {
+      pagination: { pageIndex: pageIndex - 1, pageSize },
+      globalFilter,
+    },
+    manualPagination: true,
+    onPaginationChange: (updater) => {
+      const next =
+        typeof updater === "function"
+          ? updater({ pageIndex: pageIndex - 1, pageSize })
+          : updater;
+      setPageIndex(next.pageIndex + 1);
+      setPageSize(next.pageSize);
+    },
+    onGlobalFilterChange: setGlobalFilter,
     getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
-    onPaginationChange: setPagination,
-    state: {
-      pagination,
-    },
   });
 
   return (
     <Sidebar>
       <div className="flex flex-col w-full">
-        {/* Header */}
-        <div className="w-full">
-          <AppHeader />
-        </div>
+        <AppHeader />
 
         <div className="p-6 w-full h-full">
-          <div className="w-full flex justify-between">
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-              Mudir Atau Pimpinan
-            </h3>
-
-            <div>
-              <ModalTambahMudirAtauPimpinan
-                openDialogEditMudirAtauPimpinan={
-                  openDialogEditMudirAtauPimpinan
-                }
-                setOpenDialogEditMudirAtauPimpinan={
-                  setOpenDialogEditMudirAtauPimpinan
-                }
-              />
-            </div>
+          <div className="flex justify-between mb-4">
+            <h3 className="text-2xl font-semibold">Mudir Atau Pimpinan</h3>
+            <ModalTambahMudirAtauPimpinan
+              openDialogEditMudirAtauPimpinan={openDialogEdit}
+              setOpenDialogEditMudirAtauPimpinan={setOpenDialogEdit}
+            />
           </div>
 
-          <div className="w-full">
-            <div className="flex items-center py-4">
-              <div className="w-full">
-                <Table>
-                  <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                      <TableRow key={headerGroup.id}>
-                        {headerGroup.headers.map((header) => (
-                          <TableHead
-                            key={header.id}
-                            className="uppercase font-bold"
-                          >
-                            {header.isPlaceholder
-                              ? null
-                              : flexRender(
-                                  header.column.columnDef.header,
-                                  header.getContext()
-                                )}
-                          </TableHead>
-                        ))}
-                      </TableRow>
-                    ))}
-                  </TableHeader>
-                  <TableBody>
-                    {table.getRowModel().rows.map((row) => (
-                      <TableRow key={row.id}>
-                        {row.getVisibleCells().map((cell) => (
-                          <TableCell key={cell.id}>
-                            {flexRender(
-                              cell.column.columnDef.cell,
-                              cell.getContext()
+          <div className="flex flex-col space-y-4">
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <TableHead
+                        key={header.id}
+                        className="uppercase font-bold"
+                      >
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
                             )}
-                          </TableCell>
-                        ))}
-                      </TableRow>
+                      </TableHead>
                     ))}
-                  </TableBody>
-                </Table>
-              </div>
-            </div>
+                  </TableRow>
+                ))}
+              </TableHeader>
 
-            <div className="flex justify-between items-center gap-2">
-              <div>
-                <span className="flex items-center gap-1">
-                  <div>Show Page</div>
-                  <strong>
-                    {table.getState().pagination.pageIndex + 1} of{" "}
-                    {table.getPageCount().toLocaleString()}
-                  </strong>
-                </span>
-              </div>
+              <TableBody>
+                {table.getRowModel().rows.map((row) => (
+                  <TableRow key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+
+            <div className="flex justify-between items-center">
+              <span>
+                Page {pageIndex} of {data?.totalPages ?? 1}
+              </span>
+
               <div className="flex gap-2">
                 <Button
-                  className="cursor-pointer"
-                  onClick={() => table.firstPage()}
-                  disabled={!table.getCanPreviousPage()}
+                  onClick={() => setPageIndex(1)}
+                  disabled={pageIndex === 1}
                 >
                   <ChevronsLeft />
                 </Button>
                 <Button
-                  className="cursor-pointer"
-                  onClick={() => table.previousPage()}
-                  disabled={!table.getCanPreviousPage()}
+                  onClick={() => setPageIndex((p) => Math.max(p - 1, 1))}
+                  disabled={pageIndex === 1}
                 >
                   <ChevronLeft />
                 </Button>
                 <Button
-                  className="cursor-pointer"
-                  onClick={() => table.nextPage()}
-                  disabled={!table.getCanNextPage()}
+                  onClick={() => setPageIndex((p) => p + 1)}
+                  disabled={pageIndex === (data?.totalPages ?? 1)}
                 >
                   <ChevronRight />
                 </Button>
                 <Button
-                  className="cursor-pointer"
-                  onClick={() => table.lastPage()}
-                  disabled={!table.getCanNextPage()}
+                  onClick={() => setPageIndex(data?.totalPages ?? 1)}
+                  disabled={pageIndex === (data?.totalPages ?? 1)}
                 >
                   <ChevronsRight />
                 </Button>
@@ -446,10 +223,8 @@ export default function MudirAtauPimpinan() {
         </div>
 
         <ModalHapusMudirAtauPimpinan
-          openDialogHapusMudirAtauPimpinan={openDialogHapusMudirAtauPimpinan}
-          setOpenDialogHapusMudirAtauPimpinan={
-            setOpenDialogHapusMudirAtauPimpinan
-          }
+          openDialogHapusMudirAtauPimpinan={openDialogHapus}
+          setOpenDialogHapusMudirAtauPimpinan={setOpenDialogHapus}
         />
       </div>
     </Sidebar>
